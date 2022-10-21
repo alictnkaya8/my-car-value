a3addd75b7d49b92755eafaa1a4fd423b22d1a75
alictnkaya8 <49043825+alictnkaya8@users.noreply.github.com>
2022-10-21 11:38:40 +0300

    user crud operations impelemented

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,46 @@
-import { Post, Body, Controller } from '@nestjs/common';
+import {
+  Post,
+  Get,
+  Body,
+  Controller,
+  Patch,
+  Param,
+  Query,
+  Delete,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
   constructor(private usersService: UsersService) {}
+
   @Post('signup')
   createUser(@Body() createUserDto: CreateUserDto) {
-    this.usersService.create(createUserDto.email, createUserDto.password);
+    return this.usersService.create(
+      createUserDto.email,
+      createUserDto.password,
+    );
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id));
+  }
+
+  @Get()
+  getUserByEmail(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Patch(':id')
+  updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), updateUserDto);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
@@ -12,5 +12,26 @@ export class UsersService {
   create(email: string, password: string) {
     const user = this.usersRepository.create({ email, password });
     return this.usersRepository.save(user);
+  }
+
+  findOne(id: number) {
+    return this.usersRepository.findOneBy({ id });
+  }
+
+  find(email: string) {
+    return this.usersRepository.find({ where: { email } });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) throw new NotFoundException();
+    Object.assign(user, attrs);
+    return this.usersRepository.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) throw new NotFoundException();
+    return this.usersRepository.remove(user);
   }
 }
